6a23338e4e85a27e8d1fb2f8fd3d1202eba65508
ajitsapkota <ajitsapkot2@gmail.com>
2025-08-02 11:02:37 +0545

    Initial commit

---
+++ tiled/Tilesheet/platformPack_tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="platformPack_tilesheet" tilewidth="32" tileheight="32" tilecount="392" columns="28">
+ <image source="Tilesheet/platformPack_tilesheet.png" width="896" height="448"/>
+</tileset>
